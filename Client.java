7736c599009a291c3da79155e490fcc1b5879231
RootOfz <mfsa17@student.aau.com>
2018-10-24 16:14:37 +0200

    moved all thread parts to Client class

--- Client.java
+++ Client.java
@@ -5,6 +5,7 @@ import java.io.ObjectInputStream;
 import java.io.ObjectOutputStream;
 import java.net.Socket;
 import java.net.UnknownHostException;
+import java.util.ArrayList;
 import java.util.Scanner;
 import java.io.DataInputStream;
 import java.io.DataOutputStream;
@@ -22,13 +23,84 @@ public class Client {
 	public static ObjectOutputStream objectOutput;
 	public static Socket socket;
 	static Scanner scan = new Scanner(System.in);
-	static boolean chatroom;
-	static boolean loggedIn;
+	static boolean chatroom = false;
+	static boolean loggedIn = false;
+	static boolean lobbyOpen = false;
 	
 	
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-		LoginUI login = new LoginUI();
+		if(!loggedIn)
+		{
+			LoginUI login = new LoginUI();
+		}
+		else if(lobbyOpen)
+		{
+			LobbyUI Lobby = new LobbyUI();
+		}
+		else
+		{
+			ChatUI chatroom = new ChatUI(LobbyUI.newChatroomName);
+		}
+		
+    	new Thread(() -> { //thread to recieve information from server
+
+	        while (true){
+	        	           
+
+                //Get the input from the client
+                ArrayList<String> Input = null;
+
+                try {
+                    Input = (ArrayList<String>) Client.objectInput.readObject();
+                } catch (ClassNotFoundException | IOException e) {
+                    e.printStackTrace();
+                    continue;
+                }
+
+                //If null skip checking for stuff
+                if (Input == null)
+                    continue;
+                if(chatroom)
+                {
+                if (Input.get(0).equals("NEW MESSAGE")) {
+
+                	
+	 					try {
+							ChatUI.getMessages(Input);
+						} catch (ClassNotFoundException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} catch (IOException e) {
+							// TODO Auto-generated catch block
+							e.printStackTrace();
+						} //runs the method for printing the new message
+	 					
+	 					//Input = null; //empty input after message is recieved
+	                
+                	} 
+                }
+                else if(lobbyOpen)
+                {
+                		LobbyUI.PeopleOnline();
+                		LobbyUI.chatOnline();
+                		LobbyUI.frame.revalidate();
+        	        	LobbyUI.frame.repaint();
+                }
+	        	//getOnlineUsers(); //add to UI of chatroom
+                	
+                	try {
+    					Thread.sleep(50);
+    				} catch (InterruptedException e) {
+    					// TODO Auto-generated catch block
+    					e.printStackTrace();
+    				}
+}
+
+		}).start();
+		// login vindue skal lukkes når lobby åbnes
+		// lobby vindue skal lukkes når chatroom åbnes
+		// når chatroom lukkes åbnes skal lobby åbnes
 		
 	}//end of main
 	
--- LobbyUI.java
+++ LobbyUI.java
@@ -14,9 +14,9 @@ public class LobbyUI extends JPanel{
 //	private BufferedReader in;
 //    private PrintWriter out;
 	//Setup of the program window
-    private JFrame frame = new JFrame("Chat Frame"); 
-    private JPanel POnline = new JPanel();
-    private JPanel COnline = new JPanel();
+    public static JFrame frame = new JFrame("Chat Frame"); 
+    private static JPanel POnline = new JPanel();
+    private static JPanel COnline = new JPanel();
     private JPanel CreateChatPanel = new JPanel();
     
 	public LobbyUI() {
@@ -74,27 +74,12 @@ public class LobbyUI extends JPanel{
         
         
 		//Run the client requesting on another thread.
-		new Thread(() -> {
-
-	        while (true){
-	        	PeopleOnline();
-	        	chatOnline();
-	        	frame.revalidate();
-	        	frame.repaint();
-	        	try {
-					Thread.sleep(50);
-				} catch (InterruptedException e) {
-					// TODO Auto-generated catch block
-					e.printStackTrace();
-				}
-	        }
-
-		}).start();
+		
        
 	}
 	
     
-    public void PeopleOnline() {
+    public static void PeopleOnline() {
     	//Get array of the usernames from the server
     	ArrayList<String> UserName = getOnlineUsers();
     	POnline.removeAll();
@@ -118,7 +103,8 @@ public class LobbyUI extends JPanel{
 				if (CreateChatroomMethod(CreateChatroomTextField.getText())) {
 						
 					ChatUI chatroom = new ChatUI(CreateChatroomTextField.getText());
-				
+					Client.chatroom = true;
+					Client.lobbyOpen = false;
 				}
 			}          
 	    });
@@ -151,7 +137,7 @@ public class LobbyUI extends JPanel{
     	return false;
     }
     
-    public ArrayList<String> getOnlineUsers() {
+    public static ArrayList<String> getOnlineUsers() {
     	try {
     		ArrayList<String> command = new ArrayList<String>();
     		command.add("GET ONLINE USERS");
@@ -169,7 +155,7 @@ public class LobbyUI extends JPanel{
     	return null;
     }
     
-    public ArrayList<String> getChatrooms() {
+    public static ArrayList<String> getChatrooms() {
     	try {
     		ArrayList<String> command = new ArrayList<String>();
     		command.add("GET CHATROOMS");
@@ -187,7 +173,7 @@ public class LobbyUI extends JPanel{
     	return null;
     }
     
-    public void joinChatroom(String ChatroomName) {
+    public static void joinChatroom(String ChatroomName) {
     	try {
     		ArrayList<String> command = new ArrayList<String>();
     		command.add("JOIN CHATROOM");
@@ -201,8 +187,9 @@ public class LobbyUI extends JPanel{
     }
    
     static int i = 0;
-    ArrayList<String> ChatroomNames = null;
-    public void chatOnline() {
+    static ArrayList<String> ChatroomNames = null;
+	public static String newChatroomName;
+    public static void chatOnline() {
     	//Get array of the chat rooms from the server
     	ChatroomNames = getChatrooms();
     	COnline.removeAll();
@@ -218,6 +205,8 @@ public class LobbyUI extends JPanel{
     			public void actionPerformed(ActionEvent arg0) {
     				joinChatroom(ChatroomNames.get(roomID));
     				ChatUI chatroom = new ChatUI(ChatroomNames.get(roomID));
+    				Client.chatroom = true;
+					Client.lobbyOpen = false;
     			}          
     	    });
     	}
