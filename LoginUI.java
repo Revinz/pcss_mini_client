72ae935d74cd31ef9a9c8f8836388c44fc2eb408
Powder6 <36990223+Powder6@users.noreply.github.com>
2018-10-25 07:25:42 +0200

    updagt LoginUI

--- LoginUI.java
+++ LoginUI.java
@@ -1,54 +1,42 @@
-import java.awt.BorderLayout;
-import java.awt.GridBagConstraints;
-import java.awt.GridBagLayout;
-import java.awt.Insets;
-
-
-
+﻿import javax.swing.*;
+import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
 import java.io.IOException;
-import java.io.ObjectInputStream;
-import java.io.ObjectOutputStream;
+import java.io.PrintStream;
+import java.io.PrintWriter;
 import java.net.Socket;
-import java.net.UnknownHostException;
-import java.util.ArrayList;
 
-import javax.swing.JButton;
-import javax.swing.JFrame;
-import javax.swing.JLabel;
-import javax.swing.JPanel;
-import javax.swing.JTextArea;
-import javax.swing.JTextField;
-import javax.swing.SwingUtilities;
-import javax.swing.UIManager;
-
-class LoginUI {
-    boolean     loggedIn = false;
-    String      appName     = "Login you piece of shit";
+public class LoginUI {
+    String  username;
+    String  password;
+    String      appName     = "Login";
     JFrame      newFrame    = new JFrame(appName);
-    JButton     sendMessage;
-    JTextField  messageBox;
-    JTextArea   chatBox;
     JTextField  usernameChooser;
     JTextField  passwordChooser;
     JFrame      preFrame;
-    
-    LoginUI() {
-    	
+
+
+    public static void main(String[] args) {
+        SwingUtilities.invokeLater(new Runnable() {
+            @Override
+            public void run() {
                 try {
                     UIManager.setLookAndFeel(UIManager
                             .getSystemLookAndFeelClassName());
                 } catch (Exception e) {
                     e.printStackTrace();
                 }
-                preDisplay();
+                LoginUI mainGUI = new LoginUI();
+                mainGUI.loginUI();
+            }
+        });
+
     }
 
 
-
     //this is the first GUI  "username and password"
-    public void preDisplay() {
+    public void loginUI() {
         newFrame.setVisible(false);
         preFrame = new JFrame(appName);
         usernameChooser = new JTextField(15);
@@ -95,11 +83,6 @@ class LoginUI {
     }
 
 
-
-    String  username;
-    String  password; // This is the IP-adress
-
-    // This register if the username and/or IP works or not
     class enterServerButtonListener implements ActionListener {
         public void actionPerformed(ActionEvent event) {
             username = usernameChooser.getText();
@@ -107,22 +90,28 @@ class LoginUI {
             if ((password.length() < 1) || (username.length() < 1)) {
                 System.out.println("Enter a valid username or a IP-address");
             } else {
-            	Client.hostName = password;
-            	try {
-            		Client.socket = new Socket(Client.hostName, Client.port);
-            		Client.objectInput = new ObjectInputStream(Client.socket.getInputStream());
-            		Client.objectOutput = new ObjectOutputStream(Client.socket.getOutputStream());
-            		ArrayList<String> command = new ArrayList<String>();
-            		command.add(username);
-            		Client.objectOutput.writeObject(command);
-        			Client.objectOutput.flush();
-        			LobbyUI Lobby = new LobbyUI();
-            		}
-            		catch (UnknownHostException e) {
-                        System.err.println("Don't know about host: taranis");
-                    } catch (IOException e) {
-                        System.err.println("Couldn't get I/O for the connection to:" + Client.hostName);
-                    }
+                String serverAddress = JOptionPane.showInputDialog(
+                        preFrame,
+                        "Enter IP Address of the Server:",
+                        JOptionPane.QUESTION_MESSAGE);
+
+                Socket usernameSocket = null;
+                try {
+                    usernameSocket = new Socket(serverAddress, 4009);
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                PrintStream OUT = null;
+                try {
+                    OUT = new PrintStream(usernameSocket.getOutputStream());
+                } catch (IOException e) {
+                    e.printStackTrace();
+                }
+                OUT.println(username);
+
+                //end the display
+                preFrame.setVisible(false);
+
             }
         }
 
